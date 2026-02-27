bcf2d909383eafded4107d7efc40a8a4f5a25996
MAdisrya <mario.adisurya@gmail.com>
2026-02-28 09:16:43 +1300

    fix(internal): formatting

--- lib/checkout_intents/models/billing_get_balance_response.rb
+++ lib/checkout_intents/models/billing_get_balance_response.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
 module CheckoutIntents
@@ -15,22 +14,3 @@ module CheckoutIntents
     end
   end
 end
-||||||| parent of 59fce0d (chore(internal): improvements)
-=======
-# frozen_string_literal: true
-
-module CheckoutIntents
-  module Models
-    # @see CheckoutIntents::Resources::Billing#get_balance
-    class BillingGetBalanceResponse < CheckoutIntents::Internal::Type::BaseModel
-      # @!attribute balance
-      #
-      #   @return [CheckoutIntents::Models::Money]
-      required :balance, -> { CheckoutIntents::Money }
-
-      # @!method initialize(balance:)
-      #   @param balance [CheckoutIntents::Models::Money]
-    end
-  end
-end
->>>>>>> 59fce0d (chore(internal): improvements)
